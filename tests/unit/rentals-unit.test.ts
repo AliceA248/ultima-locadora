87f30ddf8de977ca1693b6919d3cea9f34d2e016
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-07-19 21:14:21 -0300

    primeiro commit

---
+++ tests/unit/rentals-unit.test.ts
@@ -0,0 +1,12 @@
+
+describe("Rentals Service Unit Tests", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  })
+})
+
+describe("test voucher", () => {
+  it("create voucher", async () => {
+    jest.spyOn()
+  })
+} )
